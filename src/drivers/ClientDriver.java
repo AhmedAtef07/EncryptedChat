d778532fe4b204fa68931c9ad992345e59db2db0
Ahmed Atef <ahmedatef07@hotmail.com>
2016-11-28 15:05:28 +0200

    Receive server ip from the command line.

--- src/drivers/ClientDriver.java
+++ src/drivers/ClientDriver.java
@@ -12,17 +12,20 @@ import user_interface.CommandLineDisplay;
 public class ClientDriver {
 
     private static final int PORT = 47360;
-    private static final String IP = "localhost";
+    private static String ip = "localhost";
 
     public static void main(String args[]) throws IOException, InvalidKeySpecException,
             NoSuchAlgorithmException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        System.out.print("Insert the server ip: ");
+        ip = br.readLine();
+
         System.out.print("Insert a username: ");
         String username = br.readLine();
 
 
-        ChatClient chatClient = new ChatClient(IP, PORT, username, new CommandLineDisplay());
+        ChatClient chatClient = new ChatClient(ip, PORT, username, new CommandLineDisplay());
 
         while (true) {
             chatClient.sendTextToServer(br.readLine());
